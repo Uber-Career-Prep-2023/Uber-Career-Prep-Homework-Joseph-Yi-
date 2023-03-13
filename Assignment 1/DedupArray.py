ecb9627026b5feeb783d06a8e998ac6e9cb32cad
JosephYi29 <josephcrest29@gmail.com>
2023-03-13 04:45:58 -0400

    March 13th 4:45am

--- Assignment 1/DedupArray.py
+++ Assignment 1/DedupArray.py
@@ -1,13 +1,32 @@
+# Sort the array then solve
+
+# Time complexity is O(n)
+# the function only itterates through the loop once
+
+# Space Complexity is O(1)
+# the only extra space required are some variables
+# which would be the same size independent of the size of input
+
+#   Approach
+#   1. Check if the array's last value is repeated 
+#   2. Deletes if not unique
+
+# this took me 5 min
 
 def dedup_array(arr):
     if not arr:
         return []
-    last_unique = arr[0]
+    repeat = arr[0]
     i = 1
+
+    # loops through array
     while i < len(arr):
-        if arr[i] == last_unique:
+
+        # checks if the value is repeated
+        if arr[i] == repeat:
             del arr[i]
         else:
-            last_unique = arr[i]
+            # changes the new repeated value
+            repeat = arr[i]
             i += 1
     return arr
