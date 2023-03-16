2b1db1a1354b94bfb6e747764a81f13012346001
JosephYi29 <josephcrest29@gmail.com>
2023-03-15 22:49:11 -0400

    March 15th 10:49 pm

--- Assignment 1/KAnagrams.py
+++ Assignment 1/KAnagrams.py
@@ -1,21 +1,60 @@
+# Hashing the elements
+
+# Time complexity is O(m + n)
+# the function loops through both the first and second strings 
+# once. Therefore the time complexity is dependent on the sum of the
+# length of those two strings
+
+# Space Complexity is O(1)
+# given that there are only 128 possible characters that 
+# can be used, the space used is constant relative to 
+# the length of either string
+
+#   Approach
+#   1. useing an array of frequencies to record the frequency of 
+#.     letters in first string
+#   2. decrement the frequency of each character as processed in second string
+#   3. if the difference of characters is greater than K return false
+#   4. return tue otherwise
 def KAnagrams(s1, s2, k):
 
+    # checks for uneven lengths
     if len(s1) != len(s2):
         return False
 
-    char_count = [0] * 26
-    for char in s1:
-        char_count[ord(char) - ord('a')] += 1
+    # frequency table
+    freq = [0] * 128
 
-    diff_count = 0
-    for char in s2:
-        idx = ord(char) - ord('a')
-        char_count[idx] -= 1
-        if char_count[idx] < 0:
-            diff_count += 1
-        if diff_count > k:
+    # indexes all the characters of the first string
+    for curr in s1:
+        freq[ord(curr)] += 1
+
+    diff = 0
+    # compares frequencies to the other string
+    for curr in s2:
+
+        freq[ord(curr)] -= 1
+
+        # if there is a difference, record as a difference
+        if freq[ord(curr)] < 0:
+            diff += 1
+        # check if there are two many differences
+        if diff > k:
             return False
 
     return True
 
+# False
+print(KAnagrams("apple", "peach", 1))
+
+# True
+print(KAnagrams("apple", "peach", 2))
+
+# True
+print(KAnagrams("cat", "dog", 3))
     
+# True
+print(KAnagrams("debit curd", "bad credit", 1))
+
+# False
+print(KAnagrams("baseball", "basketball", 2))
--- Assignment 1/ShortestSubstring.py
+++ Assignment 1/ShortestSubstring.py
@@ -1,49 +1,45 @@
-def shortestSubstring(s, required):
-    # Initialize a dictionary to keep track of the required characters and their counts
-    req_counts = {}
-    for c in required:
-        req_counts[c] = req_counts.get(c, 0) + 1
+# Two pointers
 
-    # Initialize variables to keep track of the substring
-    start = 0
-    end = 0
-    curr_counts = {}
-    count = len(required)
-    min_len = float('inf')
+# Time complexity is O(n)
+# the loop itterates through the entire list once
 
-    # Move the end pointer to find a valid substring
-    while end < len(s):
-        curr_counts[s[end]] = curr_counts.get(s[end], 0) + 1
-        if s[end] in req_counts and curr_counts[s[end]] <= req_counts[s[end]]:
-            count -= 1
-        end += 1
+# Space complexity is O(1)
+# since the only additional space required is the dictionary 
+# for the number of characters, there can only be 128 possible 
+# characters
 
-        # Move the start pointer to shrink the substring
-        while count == 0:
-            if end - start < min_len:
-                min_len = end - start
-            curr_counts[s[start]] -= 1
-            if s[start] in req_counts and curr_counts[s[start]] < req_counts[s[start]]:
-                count += 1
-            start += 1
+#   Approach
+#   1. Created a set out of the list of required characters
+#   2. creates a growing and shrinking window.
+#   3. checks the substring if it contains all of the requried characters
+#   4. compares the length of this substring and records the minimum
 
-    return min_len if min_len != float('inf') else 0
+# this took 35 min
+def shortestSubstring(s, req):
+    required_chars = set(req)
+    freq = {}
+    left = 0
+    right = 0
+    minLen = float('inf')
 
+    while right < len(s):
+        curr = s[right]
+        freq[curr] = freq.get(curr, 0) + 1
+        right += 1
 
+        while all(freq.get(curr, 0) >= 1 for curr in required_chars):
+            minLen = min(minLen, right - left)
+            left_char = s[left]
+            freq[left_char] -= 1
+            left += 1
 
+    return minLen if minLen != float('inf') else 0
 
 # 4
-print(shortestSubstring('ADOBECODEBANC', 'ABC'))
-
-# 5
-print(shortestSubstring('this is a test string', 'tist'))
+print(shortestSubstring("abracadabra", "abc"))
 
 # 3
-print(shortestSubstring('geeksforgeeks', 'ork'))
+print(shortestSubstring("zxycbaabcdwxyzzxwdcbxyzabccbazyx", "zzyzx"))
 
-# 1
-print(shortestSubstring('aaaa', 'a'))
-
-# 0
-print(shortestSubstring('abcdefg', 'hijklmn'))
-
+# 3
+print(shortestSubstring("dog", "god"))
