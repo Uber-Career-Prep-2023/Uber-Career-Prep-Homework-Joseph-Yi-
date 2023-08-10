f862cd7f37eed71599d15c32205b80cb83e37fd2
JosephYi29 <josephcrest29@gmail.com>
2023-08-09 20:44:34 -0400

    Running Median Complete

--- Assignment 4/RunningMedian.py
+++ Assignment 4/RunningMedian.py
@@ -1,18 +1,24 @@
 from heapq import *
 # Main two heaps
 
-# Time Complexity is O(N log N)
-# 
+# Time Complecurrity is O(N log N)
+# This function utilizes heaps to maintain a heap
+# of elements that are greater than the median and another
+# that is less than the median. There are N elements with each taking
+# log(n) time to store into the heap resulting in 
+# n log(n) time. 
 
-# Space Complexity is O(N)
-# 
+# Space Complecurrity is O(N)
+# The two heaps store half of the entries in the array each
+# making the total amount of ecurrtra data being equal to the size 
+# of the input array
 
 # This took me 17 min
 
 # this function takes in an array as the strean
 def runningMedian(arr, n):
 
-    # max heap to store the smaller half elements
+    # macurr heap to store the smaller half elements
     s = []
 
     # min heap to store the greater half elements
@@ -27,33 +33,33 @@ def runningMedian(arr, n):
     print(med)
  
     for i in range(1, n):
-        x = arr[i]
+        curr = arr[i]
  
         # left side heap has more elements
         if len(s) > len(g):
-            if x < med:
+            if curr < med:
                 heappush(g, heappop(s))
-                heappush(s, x)
+                heappush(s, curr)
             else:
-                heappush(g, x)
+                heappush(g, curr)
             med = (nlargest(1, s)[0] + nsmallest(1, g)[0])/2
  
         # both heaps are balanced
         elif len(s) == len(g):
-            if x < med:
-                heappush(s, x)
+            if curr < med:
+                heappush(s, curr)
                 med = nlargest(1, s)[0]
             else:
-                heappush(g, x)
+                heappush(g, curr)
                 med = nsmallest(1, g)[0]
  
 		# right side heap has more elements
         else:
-            if x > med:
+            if curr > med:
                 heappush(s, heappop(g))
-                heappush(g, x)
+                heappush(g, curr)
             else:
-                heappush(s, x)
+                heappush(s, curr)
             med = (nlargest(1, s)[0] + nsmallest(1, g)[0])/2
  
         print(med)
